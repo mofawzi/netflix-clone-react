e6a4c5b462bd65eba2af69ecbef1a52dfe0bae25
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-04-01 17:58:46 +0200

    Update Rows

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ function App() {
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="History Movies" fetchUrl={requests.fetchHistoryMovies} />
+      <Row title="Animation Movies" fetchUrl={requests.fetchAnimationMovies} />
+      <Row title="Fantasy Movies" fetchUrl={requests.fetchFantasyMovies} />
+      <Row title="Science Fiction Movies" fetchUrl={requests.fetchSiFiMovies} />
     </div>
   );
 }
