8192530148e3f01a7a60c084f64de1c6b795029b
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-04-01 17:40:58 +0200

    Update Requests

--- src/requests.js
+++ src/requests.js
@@ -3,7 +3,9 @@ const API_KEY = "f5f2e9501be00d5965646e25f154ba92";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&language=en-US&page=1&timezone=America%2FNew_York&sort_by=popularity.desc&include_null_first_air_dates=false`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   fetchHistoryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=36`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
   fetchFantasyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=14`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
