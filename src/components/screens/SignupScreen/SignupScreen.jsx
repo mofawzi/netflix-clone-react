de0191841b010c70a90da87524f18093c5808ced
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-04-07 21:06:38 +0200

    Update Signup screen

--- src/components/screens/SignupScreen/SignupScreen.jsx
+++ src/components/screens/SignupScreen/SignupScreen.jsx
@@ -1,17 +1,27 @@
 import './SignupScreen.css'
 
 const SignupScreen = () => {
+
+  const register = e => {
+    e.preventDefault();
+
+  }
+
+  const signIn = e => {
+    e.preventDefault();
+    
+  }
+
   return (
     <div className="signupScreen">
       <form >
         <h1>Sign In</h1>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
-        <h4><span className="signupScreen__gray">New to Netflix? </span>
-        <span className="signupScreen__link">
-           Sign Up now.
-        </span>
+        <button type="submit" onClick={signIn}>Sign In</button>
+        <h4>
+          <span className="signupScreen__gray">New to Netflix? </span>
+          <span onClick={register} className="signupScreen__link"> Sign Up now.</span>
         </h4>
       </form>
     </div>
